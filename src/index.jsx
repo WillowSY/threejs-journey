c6fd8c1c108d459225c8db45821707207eaf11ec
LSY <kongma0312@gmail.com>
2024-01-22 22:12:56 +0900

    Complete Player

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
-import KeyboardControls from '@react-three/drei'
+import {KeyboardControls} from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
